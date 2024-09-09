75f3bc4ef96b9f4d9d292c950e6fbac02a77acdf
sarsdev <sarsdesenv@gmail.com>
2024-09-09 19:55:04 -0300

    Adicionado o /api na URL e adicionada a rota de apresentação

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const app = express();
 const porta = 3000;
 
 app.use(express.json());
-app.use('/usuario', rotas.usuario);
+app.use('/api', rotas.apresentacao);
+app.use('/api/usuario', rotas.usuario);
 
 app.listen(porta, () => {
   console.log(`App na URL http://localhost:${porta}`);
